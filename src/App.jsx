31f4aea524fa491ed06a29a1c53789a3545a3aea
Carlos Gabriel Soares <cgvilasnovas@gmail.com>
2021-09-02 11:33:40 -0300

    Try to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
 import * as React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import ProTip from "./ProTip";
+import {ThemeProvider} from '@material-ui/core/styles';
+import theme from './theme'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v5-beta example with styled-components
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <div color="primary" >Isso deveria estar colorido</div>
+        <h1>Esse é um teste H1</h1>
+        <h2>Esse é um teste H2</h2>
+        <h3>Esse é um teste H3</h3>
+        <h4>Esse é um teste H4</h4>
+        
+      </ThemeProvider>
+      
+    </React.StrictMode>
   );
 }
